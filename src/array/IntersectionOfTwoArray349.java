8138cd0c0437ff2a3126dd87bba2d608ad56619d
shuangyao <shuangyao@sohu-inc.com>
2016-07-29 16:52:52 +0800

    update

---
+++ src/array/IntersectionOfTwoArray349.java
@@ -0,0 +1,26 @@
+package array;
+
+/**
+ * Created with by shuangyao on 2016/7/29.
+ */
+
+/**
+ * Given two arrays, write a function to compute their intersection.
+ * Example: Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+ * Note:
+ *      1. Each element in the result must be unique.
+ *      2. The result can be in any order.
+ */
+public class IntersectionOfTwoArray349 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null){
+            return null;
+        }
+        if (nums1.length == 0 || nums2.length == 0){
+            return new int[0];
+        }
+
+
+    }
+}
