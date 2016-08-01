0237d7d467893dfeab7613d59113f20dc543cb70
shuangyao <shuangyao@sohu-inc.com>
2016-08-01 17:06:05 +0800

    237, 349

--- src/array/IntersectionOfTwoArray349.java
+++ src/array/IntersectionOfTwoArray349.java
@@ -4,6 +4,11 @@ package array;
  * Created with by shuangyao on 2016/7/29.
  */
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Given two arrays, write a function to compute their intersection.
  * Example: Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
@@ -17,10 +22,23 @@ public class IntersectionOfTwoArray349 {
         if (nums1 == null || nums2 == null){
             return null;
         }
-        if (nums1.length == 0 || nums2.length == 0){
-            return new int[0];
+
+        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> intersection = new HashSet<Integer>();
+        for (int num : nums1){
+            set.add(num);
+        }
+        for (int num : nums2){
+            if (set.contains(num)){
+                intersection.add(num);
+            }
         }
 
-
+        int[] result = new int[intersection.size()];
+        int index = 0;
+        for (int num : intersection){
+            result[index++] = num;
+        }
+        return result;
     }
 }
