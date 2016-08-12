8657855b4b5609c11f3e3caec9be5b8b9942d4b9
shuangyao <shuangyao@sohu-inc.com>
2016-08-12 14:17:34 +0800

    326

--- src/number/PowerOdThree326.java
+++ src/number/PowerOdThree326.java
@@ -11,14 +11,11 @@ package number;
 public class PowerOdThree326 {
 
     public boolean isPowerOfThree(int n) {
-        return n > 0 && ((int)Math.pow(3, Math.round(Math.log(n) / Math.log(3)))) == n;
+        return n > 0 && (Math.pow(3, (int)(Math.log(Integer.MAX_VALUE) / Math.log(3)))) % n == 0;
     }
 
     public static void main(String[] args){
         PowerOdThree326 instance = new PowerOdThree326();
-        System.out.println(instance.isPowerOfThree(243));
-        System.out.println(Math.log(243));
-        System.out.println(Math.log(3));
-        System.out.println((Math.log(243) / Math.log(3)));
+        System.out.println(instance.isPowerOfThree(80));
     }
 }
