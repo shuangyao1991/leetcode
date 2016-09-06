c4f5f004e928b21f151800d13538e89f9350c406
shuangyao <shuangyao@sohu-inc.com>
2016-09-06 09:03:45 +0800

    update

--- src/string/RegularExpression10.java
+++ src/string/RegularExpression10.java
@@ -18,23 +18,31 @@ package string;
 public class RegularExpression10 {
 
     public boolean isMatch(String s, String p) {
-        if (p == null || p.length() == 0){
-            return (s == null && p == null) ||
-                    (s.length() == 0 || p.length() == 0);
+        if ((s == null && p == null) || (s.length() == 0 && p.length() == 0)) return true;
+        if (p == null || p.length() == 0 || s == null || s.length() == 0){
+            return false;
         }
 
         int i = 0, j = 0;
-        while (j < p.length()){
+        while (j < p.length() && i < s.length()){
             if (j < p.length() - 1 && p.charAt(j + 1) == '*'){
-                if (p.charAt(j) == '.'){
-                    while (i < s.length()){
-                        if (isMatch(s.substring(i), p.substring(j)))
+                if (j + 2 >= p.length()){
+                    if (p.charAt(j) == '.'){
+                        return true;
+                    }else if (p.charAt(j) == s.charAt(i)){
+                        while (i < s.length() - 1){
+                            if (s.charAt(i) != s.charAt(i+1)){
+                                return false;
+                            }
+                        }
+                        return true;
+                    }else {
+                        return false;
                     }
                 }
                 if (p.charAt(j) == '.' || s.charAt(i) == p.charAt(j)){
                     while (i < s.length() &&
                             (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')){
-                        if ()
                         if (isMatch(s.substring(i + 1), p.substring(j + 2)))
                             return true;
                         i++;
@@ -57,5 +65,6 @@ public class RegularExpression10 {
     public static void main(String[] agrs){
         RegularExpression10 instance = new RegularExpression10();
         System.out.println(instance.isMatch("ab", ".*c"));
+//        System.out.println("a".substring(3));
     }
 }
